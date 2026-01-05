3c50d4e150da28aa8239b7816e8ff5ca8a0dea38
Andrei Lyakh <Edgar.digl@mail.ru>
2026-01-05 21:11:05 +0300

    Vacancy and user models were created

--- vacancies/models.py
+++ vacancies/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Vacancy(models.Model):
+    text = models.CharField(max_length=2000)
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    age = models.IntegerField()
