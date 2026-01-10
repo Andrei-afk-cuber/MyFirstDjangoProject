f498676a19ece8330bd48a12056d08f9acf7c098
Andrei Lyakh <Edgar.digl@mail.ru>
2026-01-11 01:41:14 +0300

    Small changes.

--- vacancies/models.py
+++ vacancies/models.py
@@ -13,7 +13,7 @@ class Vacancy(models.Model):
     slug = models.SlugField(max_length=50)
     status = models.CharField(max_length=6, choices=STATUS, default="draft")
     created = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.slug
